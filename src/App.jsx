271a818360301d5ca65dd6d055e2e2a7876659e1
butterbeetle <butterbeetle@naver.com>
2024-05-10 12:30:41 +0900

    Setting. 기본 파일 업로드

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import styles from "./css/App.module.css";
+
+import Header from "./Layout/Header/Header";
+import Main from "./Layout/Main/Main";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
